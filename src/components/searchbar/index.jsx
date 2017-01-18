a8098d8c57db16e36dce13fcd2583211f66a5bdb
yangquan <quan.yang@ele.me>
2017-01-18 13:48:14 +0800

    update searchbar style

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -10,15 +10,18 @@ class Searchbar extends React.Component {
     this.state = {
       visibleClear: false,
       visibleCancel: false,
+      visibleOverlay: false,
       value: '',
     }
   }
 
   handleChange (e) {
     const v = e.target.value;
+    const bool = v ? true : false;
     this.setState({
       value: v,
-      visibleClear: true,
+      visibleClear: bool,
+      visibleOverlay: !bool,
     });
     this.props.onChange(v);
   }
@@ -30,49 +33,90 @@ class Searchbar extends React.Component {
       value: '',
     });
     this.props.onChange();
+    this.removeOverlay();
   }
 
-  handleFocus () {
-    this.setState({
-      visibleClear: false,
-      visibleCancel: true,
-    });
+  removeOverlay () {
+    var overlay = this.refs.overlay;
+    if (overlay && overlay.className.indexOf("fadeOut") === -1){
+      overlay.addEventListener("animationend",() => {
+        this.setState({
+          visibleOverlay: false,
+        });
+      }, false);
+    }
+  }
+
+  handleFocus (e) {
+    if (!e.target.value) {
+      this.setState({
+        visibleClear: false,
+        visibleCancel: true,
+        visibleOverlay: true,
+      });
+    }
   }
 
   handleClear () {
     this.setState({
       visibleClear: false,
+      visibleOverlay: true,
       value: '',
     });
     this.props.onChange();
   }
 
+  handleTouchStart (e) {
+    e.preventDefault();
+  }
+
+  getOverlay (className) {
+    return (
+      <div 
+        onTouchStart={this.handleTouchStart.bind(this)}
+        className={'searchbar-overlay ' + className} 
+        ref="overlay">
+      </div>
+    );
+  }
+
   render () {
     let style = this.state.visibleCancel ? { marginRight: '0px' } : null;
+    let overlay = null;
+    if (this.state.visibleOverlay ) {
+      if (this.state.visibleCancel ) {
+        overlay = this.getOverlay('fadeIn');
+      } else {
+        overlay = this.getOverlay('fadeOut');
+      }
+    }
     return (
-      <div className="ui-searchbar">
-        <div className="search-input">
-          <input 
-            value={this.state.value}
-            type="text" 
-            placeholder="搜索" 
-            onChange={this.handleChange.bind(this)}
-            onFocus={this.handleFocus.bind(this)}
-          />
-          {
-            this.state.visibleClear ? 
-              <span 
-                onClick={this.handleClear.bind(this)} 
-                className="icon-clear">
-              </span> : null
-          }
+      <div className="ui-searchbar-wrapper">
+        <div className="ui-searchbar">
+          <div className="search-input">
+            <input 
+              value={this.state.value}
+              type="text" 
+              placeholder="搜索" 
+              onChange={this.handleChange.bind(this)}
+              onFocus={this.handleFocus.bind(this)}
+            />
+            {
+              this.state.visibleClear ? 
+                <span 
+                  onClick={this.handleClear.bind(this)} 
+                  className="icon-clear">
+                </span> : null
+            }
+          </div>
+          <span 
+            onClick={this.handleCancel.bind(this)} 
+            style={style}
+            className="cancel">
+              取消
+          </span>
         </div>
-        <span 
-          onClick={this.handleCancel.bind(this)} 
-          style={style}
-          className="cancel">
-            取消
-        </span>
+        { overlay }
       </div>
     )
   }
--- src/demo/searchbar/index.jsx
+++ src/demo/searchbar/index.jsx
@@ -10,10 +10,29 @@ export default class SearchbarDemo extends React.Component {
     console.log(value)
   }
 
+  celick () {
+    alert(1);
+  }
+
   render() {
     return (
       <div className="searchbar-demo">
         <Searchbar onChange={this.handleChange.bind(this)} />
+        <div className="list">
+          <div className="item">
+            <label className="item-label">
+              picker demo
+            </label>
+          </div>
+        </div>
+
+        <div className="list">
+          <div className="item">
+            <label className="item-label" onClick={this.celick.bind(this)}>
+              tost demo
+            </label>
+          </div>
+        </div>
       </div>
     )
   }
